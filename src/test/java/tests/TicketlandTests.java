cece0d74a887179da61f7ba2d8035bce7f98e254
Anna Gasnikova <however@bk.ru>
2026-09-22 20:03:59 +0400

    Added 3 tests of critical user journey

--- src/test/java/tests/TicketlandTests.java
+++ src/test/java/tests/TicketlandTests.java
@@ -3,32 +3,71 @@ package tests;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byPlaceholder;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.WebDriverConditions.urlContaining;
+import static com.codeborne.selenide.WebDriverConditions.urlStartingWith;
 
 
 public class TicketlandTests extends TicketlandBase {
     @Test
-    void succesTicketlandSearchTest() {
-        open (baseUrl);
+    void mainPageShouldOpen() {
+        open ("");
 
         SelenideElement cookieBanner =
                 $("[aria-label='Уведомление об использовании файлов cookie']");
-        cookieBanner.$("button")
+
+        if (cookieBanner.isDisplayed()) {
+            cookieBanner.$("button").click();
+        }
+
+        webdriver().shouldHave(urlStartingWith(baseUrl));
+        $$("[aria-label='Ticketland']")
+                .findBy(visible)
+                .shouldBe(visible);
+    }
+    @Test
+    void mainPageShouldDisplayMainElements() {
+        open ("");
+
+        SelenideElement cookieBanner =
+                $("[aria-label='Уведомление об использовании файлов cookie']");
+
+        if (cookieBanner.isDisplayed()) {
+            cookieBanner.$("button").click();
+        }
+
+        $(byPlaceholder("Найти событие, артиста или место"))
+                .shouldBe(visible);
+
+        $$("img.tl-image__image[alt]")
+                .shouldBe(sizeGreaterThan(0))
+                .first()
+                .shouldBe(visible);
+    }
+    @Test
+    void eventCardShouldOpenEventPage() {
+        open("");
+
+        SelenideElement cookieBanner =
+                $("[aria-label='Уведомление об использовании файлов cookie']");
+
+        if (cookieBanner.isDisplayed()) {
+            cookieBanner.$("button").click();
+        }
+
+        $("[aria-label='Открыть Всё будет Челентано']")
                 .shouldBe(visible)
                 .click();
-//        if (cookieBanner.isDisplayed()) {
-//            cookieBanner.$("button").click();
-//        }
 
-//        $("button[class*='_search_icon_']").click();
-//        $("input[name = 'search']")
-        $(byPlaceholder("Найти событие, артиста или место"))
-                .shouldBe(visible)
-                .shouldHave(attribute("placeholder", "Найти событие, артиста или место"))
-                .setValue("концерт");
+        webdriver().shouldHave(urlContaining("/vsyo-budet-chelentano/"));
+
+
+        $("h1").shouldBe(visible)
+                .shouldHave(text("Всё будет Челентано"));
     }
+
 }
